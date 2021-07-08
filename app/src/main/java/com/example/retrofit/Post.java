cab3a58abd456237ed3f0fa24d67cea2ed253a79
mrashadyusuf <mrashadyusuf@gmail.com>
2021-07-08 23:52:24 +0800

    First commit

---
+++ app/src/main/java/com/example/retrofit/Post.java
@@ -0,0 +1,28 @@
+package com.example.retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+    private String text;
+
+    @SerializedName("body")
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
